25929bb6a8e2f68cb520fce3b1b020b252b35e03
Rohit Maurya <mauryarohitkumar36@gmail.com>
2026-08-29 09:36:58 +0530

    ReactRouter

---
+++ ReactRouter/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './component/Home'
+import About from './component/About'
+
+
+function App() {
+  
+
+  return (
+   
+    <div>
+      <BrowserRouter>
+      <Routes>
+ <Route path ="/" element = {<Home/>}/>
+ <Route path ="/about" element = {<About/>}/>
+ 
+
+      </Routes>
+     
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
