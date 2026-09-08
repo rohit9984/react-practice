67062454c1df0af8e215b61e8dc3c2e6f746ad7e
Rohit Maurya <mauryarohitkumar36@gmail.com>
2026-09-08 16:51:56 +0530

    Change Some things

--- ComponentInReact/src/main.jsx
+++ ComponentInReact/src/main.jsx
@@ -3,8 +3,41 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+function JustFunction(){
+
+  const x = "myclass";
+
+  const  myfun = () => {  
+    alert("Hello camelCash");
+
+  }
+
+  const myStyle = {
+    color: "red",
+    fontSize: "20px",
+    backgrounColor: "lightyellow"
+
+  }
+
+
+  
+  return(
+    <div>
+      <h1 style={myStyle}>Hello This is a main.jsx Component </h1>
+
+      <h2 className= {x}>My name is Rohit!</h2>
+
+       <button onClick={myfun}>Hello</button>
+    </div>
+  
+  )
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <JustFunction />
     <App />
   </StrictMode>,
 )
