b65370a83d6d8455518caa37eeba852151add9dc
Rohit Maurya <mauryarohitkumar36@gmail.com>
2026-09-20 12:42:55 +0530

    reactapp

---
+++ reactapp/src/App.js
@@ -0,0 +1,16 @@
+
+import Greeting from "./greeting";
+function App() {
+  return (
+    <div>
+      <h1>Welcome to tutorial</h1>
+    <Greeting name = "Rohit"/>
+    <Greeting name = "Laxmi"/>
+    
+
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ reactapp/src/greeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Greeting = ({name}) => {
+    return <h2>Hello, {name}!</h2>
+};
+
+export default Greeting;
